92f336edff5205e2218653aa64a829d2dc6246bd
opas-keat <opas.keat@gmail.com>
2022-12-03 08:54:39 +0700

    remove module signup

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -4,8 +4,6 @@ import '../modules/home/bindings/home_binding.dart';
 import '../modules/home/views/home_view.dart';
 import '../modules/login/bindings/login_binding.dart';
 import '../modules/login/views/login_view.dart';
-import '../modules/signup/bindings/signup_binding.dart';
-import '../modules/signup/views/signup_view.dart';
 
 part 'app_routes.dart';
 
@@ -19,11 +17,6 @@ class AppPages {
       name: _Paths.HOME,
       page: () => const HomeView(),
       binding: HomeBinding(),
-    ),
-    GetPage(
-      name: _Paths.SIGNUP,
-      page: () => const SignupView(),
-      binding: SignupBinding(),
     ),
     GetPage(
       name: _Paths.LOGIN,
--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,13 +4,11 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
-  static const SIGNUP = _Paths.SIGNUP;
   static const LOGIN = _Paths.LOGIN;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
-  static const SIGNUP = '/signup';
   static const LOGIN = '/login';
 }
